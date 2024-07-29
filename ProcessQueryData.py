e13d445d3929879574bccc4f0c81ab84183dc3ea
vedant-gupta-17 <154470051+vedant-gupta-17@users.noreply.github.com>
2024-07-29 15:49:06 -0400

    Update ProcessQueryData.py

--- ProcessQueryData.py
+++ ProcessQueryData.py
@@ -2,6 +2,8 @@ import json
 
 import pandas
 import pandas as pd
+from statsmodels.stats.multitest import multipletests
+
 from pandas import core
 import numpy as np
 import openpyxl
@@ -10,36 +12,85 @@ import requests
 import time
 from operator import getitem
 import matplotlib.pyplot as plt
-from scipy.stats import kstest
-from scipy.stats import f_oneway
+import scipy.stats as stats
 import statsmodels.api as sm
-
+from scipy import stats
+from scipy.stats import f_oneway, kstest, mannwhitneyu, false_discovery_control
 
 def main():
 
-    processData('/Users/vedant/HBB.json')
-    processData('/Users/vedant/ACE.json')
-    processData('/Users/vedant/Desktop/Translator NIH/JSON data/BRCA2.json')
-    processData('/Users/vedant/ADRB2.json')
-    processData('/Users/vedant/CYP2D6.json')
-    processData('/Users/Vedant/SMARCE1.json')
-def processData(filePath):
+    p_list = []
 
-    gene = json.loads(open(filePath).read())
+    name_list = ["BRAF", "CFTR", "DDX3Y", "PDE5A", "SLC5A2", "VKORC1",
+                 "HBB", "ACE", " BRCA2", "ADRB2", "CYP2D6", "SMARCE1"]
+
+    p_list.append(take_query('', name_list[3]))
+    p_list.append(take_query('', name_list[3]))
+    p_list.append(take_query('', name_list[3]))
+    p_list.append(take_query('', name_list[3]))
+    p_list.append(take_query('02489e70-d19e-42dd-9cf7-822fa5dfdede', name_list[4]))
+    p_list.append(take_query('caed9eff-da30-42cd-b351-669210461384', name_list[5]))
+
+    p_list.append(take_query('ebe022f4-652c-4380-b9f6-da7a3eda0887', name_list[6]))
+    p_list.append(take_query('ce531d16-f25c-4a22-a047-5909c915acc0', name_list[7]))
+    p_list.append(take_query('fc98b929-346d-479e-86c8-f056f5cca3af', name_list[8]))
+    p_list.append(take_query('854676de-c32f-4475-95fa-eac71c6e9454', name_list[9]))
+    p_list.append(take_query('58028aa9-1ba9-4022-a55d-86b7811fce3d', name_list[10]))
+    p_list.append(take_query('b47a79bd-83b5-4688-a142-e3b9642450d4', name_list[11]))
+
+    rejected, p_vals_corrected, x, y = multipletests(p_list, alpha=0.05, method="fdr_bh")
+
+
+    p_df = pd.DataFrame({"Genes": name_list, "Values":p_list, "Adjusted P Values": p_vals_corrected, "Outcome" : rejected})
+    print()
+
+def take_query(api, filePath):
+    url = "https://ars.test.transltr.io/ars/api/messages/" + api + "?trace=y"
+    r = requests.get(url, allow_redirects=True)
+    time.sleep(360)
+
+    json_data = r.json()
+    merged_version_pk = json_data['merged_version']
+    url2 = "https://ars.test.transltr.io/ars/api/messages/" + merged_version_pk
+    r2 = requests.get(url2, allow_redirects=True)
+    open('results.json', 'wb').write(r2.content)
+    gene = json.load(open('results.json', 'rb'))
+
     # Data with only results including atc_classifications
     gene_data = run(gene)
+    gene_def_data = run2(gene)
+    all_gene_scores = displayScores(gene)
+    all_gene_scores.sort()
+
     # Data with sugeno averages for items with same atc_name, also has count
     gene_stats = run_stats(gene_data)
-    print("Count and average for names:")
-    print(gene_stats)
+
+
+    #Graph of scores with atc classifications
+    plt.hist((gene_data["sugeno"]).sort_values(), bins=50)
+    plt.title("Atc Scores")
+    plt.xlabel(filePath)
+    plt.show()
+
+    #Graph without atc classifications
+    plt.hist((gene_def_data["sugeno"]).sort_values(), bins = 50)
+    plt.title("Non-Atc Scores")
+    plt.xlabel(filePath)
+    plt.show()
+
+    #Statistical Test
+
+    atc_scores = np.array(gene_data["sugeno"])
+    non_atc_scores = np.array(gene_def_data["sugeno"])
+    stat, p_value = mannwhitneyu(atc_scores, non_atc_scores)
+    print(filePath)
+    print("Statistic: ", stat)
+    print("P-Value: ", p_value)
+    print()
+
+
     # Each atc_code in the set, with its list of respective sugeno scores
     atc_codes = atc_dict(gene_data)
-
-    # KS Test of Significance
-    ks_test = run_ks(gene, atc_codes)
-    ks_df = pd.DataFrame.from_dict(ks_test, orient='index')
-    print("Dataframe Results of KS Test:")
-    print(ks_df)
 
     atc_code_index = {}
     index = 0
@@ -47,25 +98,14 @@ def processData(filePath):
         atc_code_index[index] = (code, atc_codes[code]["names"], atc_codes[code]["scores"])
         index += 1
 
-    #Analysis of Variance(Anova) Significance Test
     atc_code_df = pd.DataFrame.from_dict(atc_code_index, orient='index', columns=['code', 'names', 'scores'])
-    anova_test = anova(atc_code_df)
-    print("Anova Results:")
-    print(anova_test)
-    print()
+    return p_value
 
-def take_query(api):
-    url = "https://ars.test.transltr.io/ars/api/messages/" + api + "?trace=y"
-    r = requests.get(url, allow_redirects=True)
-    time.sleep(600)
 
-    json_data = r.json()
-    #print(json_data['merged_version'])
-    merged_version_pk = json_data['merged_version']
-    url2 = "https://ars.test.transltr.io/ars/api/messages/" + merged_version_pk
-    r2 = requests.get(url2, allow_redirects=True)
-    open('results.json', 'wb').write(r2.content)
-    json_final = r2.json
+
+
+
+
 def atc_dict(df):
     temp = {}
     for row in df.iterrows():
@@ -82,41 +122,11 @@ def atc_dict(df):
             }
 
     return temp
-
-
-def anova(atc):
-    score_list = []
-    for item in atc["scores"]:
-        score_list.append(item)
-    output = f_oneway(*score_list)
-    return output
-
-def run_ks(message, atc_res):
-    results = get_safe(message, "fields","data","message","results")
-    N = len(results)
-    result_scores = []
-    for result in results:
-        if "sugeno" in result.keys():
-            result_scores.append(result["sugeno"])
-        else:
-            print("no sugeno")
-
-    ks_list = {}
-    for code,content in atc_res.items():
-        #print(content["names"])
-        results_ks = kstest(result_scores, content["scores"])
-
-        ks_list[code] = {
-            "results": results_ks,
-            "names": content["names"]
-        }
-    return ks_list
 def run_stats(df):
     stats = {}
     temp = {}
     i = 0
     for row in df.iterrows():
-        print()
         atc_name = row[1]["atc_name"]
         sugeno = row[1]["sugeno"]
         atc_code = row[1]["atc_code"]
@@ -155,10 +165,66 @@ def loadPandas(scoreSet):
 
     df = pd.DataFrame(pandasData)
     return df
+
+def loadPandas2(scoreSet):
+    i = 1
+    pandasData = []
+
+    for item in scoreSet:
+        score = (scoreSet[item]['sugeno'])
+        name = (scoreSet[item]['node_bindings']['sn'][0]['id'])
+        data = {
+            'name' : name,
+            'sugeno' : score
+        }
+        pandasData.append(data)
+
+    df = pd.DataFrame(pandasData)
+    return df
 def run(data):
+
+    # Checks to see the the data is not empty/null
     results = getResults(data)
+
+    # Loads the knowledgeGraph from the results dictionary
     knowledgeGraph = getKnowledgeGraph(data)
+
+    # Loads the list of result names
     idlist = getresultIDs(results)
+    nodeMap = {}
+
+    # Searches the knowledge graph for the result names and then loads the nodes with that name
+    for zids in idlist:
+        for zid in zids:
+
+            nodeMap[zid] = getNode(knowledgeGraph, zid)
+
+    #Map of results with atc classifications
+    atcMap = getMaps(nodeMap)
+
+    #Returns a set of
+    pr = processResults(results, atcMap)
+
+    #Retrieves the scores with atc attributes from the processed results
+    atcResultsSet = showAtcData(pr)
+
+    #Loads score onto dataframe to return
+    dataframe = loadPandas(atcResultsSet)
+
+
+    return dataframe
+
+def run2(data):
+
+    results = getResults(data)
+
+
+    knowledgeGraph = getKnowledgeGraph(data)
+
+
+    idlist = getresultIDs(results)
+
+
     nodeMap = {}
     for zids in idlist:
         for zid in zids:
@@ -167,13 +233,17 @@ def run(data):
 
 
     atcMap = getMaps(nodeMap)
+
     pr = processResults(results, atcMap)
-    scoreSet = showData(pr)
-    dataframe = loadPandas(scoreSet)
+    atcResultsSet = showData(pr)
+    dataframe = loadPandas2(atcResultsSet)
+
+
     return dataframe
-
-
-
+def displayScores(data):
+    results = getResults(data)
+    scoreSet = getAllScores(results)
+    return scoreSet
 
 def getAtcByLevel(result, level):
     atcNames = []
@@ -203,6 +273,15 @@ def showData(pr):
     scoreSet = {}
     i = 0
     for att in pr:
+        if "atc" not in att:
+            scoreSet[i] = pr[i]
+        i += 1
+
+    return scoreSet
+def showAtcData(pr):
+    scoreSet = {}
+    i = 0
+    for att in pr:
         if "atc" in att:
             scoreSet[i] = pr[i]
         i += 1
@@ -222,6 +301,11 @@ def processResults(results, atcMap):
                         result["atc"] = atcMap[item["id"]]
     return results
 
+def getAllScores(results):
+    scores = []
+    for item in results:
+        scores.append(item["sugeno"])
+    return scores
 def getMaps(nodeMap):
     atcMap = {}
 
@@ -306,3 +390,11 @@ def get_safe(element,*keys):
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
+
+
